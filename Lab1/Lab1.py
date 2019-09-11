e080c7f630002c8aebc6332db1e711f3f2cc60d6
Unknown <justataya@gmail.com>
2019-09-11 14:31:53 +0300

    Progress commit

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -1,3 +1,18 @@
 # To add a new cell, type '#%%'
 # To add a new markdown cell, type '#%% [markdown]'
-#%% [markdown]
+#%%
+print("It works");
+#%%
+import matplotlib.pyplot as plt
+#%%
+import numpy as np
+#%%
+with open('f19.txt') as file:
+    inputData = np.array([float(val) for val in file.read().split()]) 
+
+
+time = np.arange(0,5.01, 0.01)
+plt.grid(True)
+plt.rc('figure', figsize=(30.0, 10.0))
+plt.plot(time,inputData);
+#%%
