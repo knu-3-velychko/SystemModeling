bd627dab8df2cef352bb112615b77d649bb85619
Unknown <justataya@gmail.com>
2019-10-08 12:10:31 +0300

    Finished lab1

--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -1,28 +1,28 @@
 clear
 clc
-y = dlmread('f19.txt');
+input = dlmread('f19.txt');
 dt = 0.01;
 T = 5;
 t = 0:dt:T;
 
-plot(t,y),grid;
-f = fft(y);
+plot(t,input),grid;
 
-n = length(y);
+f = fft(input);
+n = length(input);
 
 c1(1:n) = 0;
 c2(1:n) = 0;
 c(1:n) = 0;
 
 for k = 1:n
-    cos_sum = 0;
-    sin_sum = 0;
+    cosFrequency = 0;
+    sinFrequncy = 0;
     for  m = 1:n
-        cos_sum = cos_sum + y(m)*cos(2*pi*(k-1)*(m-1)/n);
-        sin_sum = sin_sum + y(m)*sin(2*pi*(k-1)*(m-1)/n); 
+        cosFrequency = cosFrequency + input(m)*cos(2*pi*(k-1)*(m-1)/n);
+        sinFrequncy = sinFrequncy + input(m)*sin(2*pi*(k-1)*(m-1)/n); 
     end
-    c1(k) = cos_sum/n;
-    c2(k) = sin_sum/n;
+    c1(k) = cosFrequency/n;
+    c2(k) = sinFrequncy/n;
     c(k) = sqrt(c1(k)*c1(k)+c2(k)*c2(k));
 end
 
@@ -32,17 +32,24 @@ plot(t,c),grid;
 figure;
 plot(abs(f)),grid;
 
+figure;
+plot(t,c),grid;
+hold on
+plot(abs(f));
+
 for k=3: n/2
-    
    if isequal(c(k), max(c(k-2:k+2)))
-       disp(k);
+       k
+       c(k)
        index=k; 
        break;
    end   
 end
 
-index=index-1
-f=index/T
+index=index-1;
+f=index/T;
+
+f
 
 TPow(1:6)=0;
 for k=1:6
@@ -59,23 +66,29 @@ A(1:5,1:5)=[TPow(6), TPow(5), TPow(4), SinPow(3), TPow(3);
             TPow(4) ,TPow(3), TPow(2), SinPow(1), TPow(1);
             SinPow(3), SinPow(2), SinPow(1), sum(sin(2*pi*f*t).^2),sum(sin(2*pi*f*t)) ; 
             TPow(3) ,TPow(2), TPow(1),sum(sin(2*pi*f*t)), n];
-disp(A);
+A
    
-C=[sum(y.*t.^3);
-   sum(y.*t.^2);
-   sum(y.*t);
-   sum(y.*sin(t.*2*pi*f));
-   sum(y)];
-disp(C);
+C=[sum(input.*t.^3);
+   sum(input.*t.^2);
+   sum(input.*t);
+   sum(input.*sin(t.*2*pi*f));
+   sum(input)];
+   
+C
 
+a=A\C;
 
-x=A\C;
+a
 
-approximation = x(1)*t.^3+x(2)*t.^2+x(3).*t+x(4)*sin(2*pi*f*t)+x(5);
+approximation = a(1)*t.^3+a(2)*t.^2+a(3).*t+a(4)*sin(2*pi*f*t)+a(5);
 figure;
 plot(t,approximation),grid;
+figure;
+plot(t,approximation),grid;
+hold on
+plot(t,input);
 err = 0;
 for i = 1:n 
-    err = err + (approximation(i)-y(i))*(approximation(i)-y(i));
+    err = err + (approximation(i)-input(i))*(approximation(i)-input(i));
 end;
 err
